800ff61eee3f0b41303c13214094dd576a2e5f72
Alessandro Varuzza <alessandrovaru@gmail.com>
2018-09-03 01:08:53 -0400

    envd

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
+    'driver'    => getenv('DB_DRIVER'),
     'host'      => getenv('DB_HOST'),
     'database'  => getenv('DB_NAME'),
     'username'  => getenv('DB_USER'),
